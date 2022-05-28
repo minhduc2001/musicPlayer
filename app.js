d12894206cbf39504cb52a7b09502cb03acd477d
ducnm46 <nduc04@gmail.com>
2022-05-28 20:25:23 +0700

    add comment

--- app.js
+++ app.js
@@ -17,25 +17,25 @@ const repeatBtn = $('.btn.btn-repeat');
 const title = $('.title')
 const playlist = $('.playlist');
 
-const api = 'https://iridescent-twilight-discovery.glitch.me/';
-let listSong = [];
+const comment = $('.comment');
+const btncomment = $('.btn-comment');
+const cmtBody = $('.comment-body');
 
+
+const api = 'http://127.0.0.1:8080/';
+let listSong = [];
+let cmt = [];
 const app = async () => {
     const getData = async () => {
         await fetch(api + 'list')
             .then(res => res.json())
             .then((res) => {
                 listSong = res
-                // listSong.pop();
                 return listSong
-            }).then(res => {
-                listSong.push(5)
-                console.log('123', listSong);
             })
         console.log('s', listSong);
     }
     await getData();
-    listSong.pop();
     // const sendRequest = async (data) => {
     //     await fetch(api + 'revc', {
     //         method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -51,11 +51,29 @@ const app = async () => {
     //         body: JSON.stringify(data) // body data type must match "Content-Type" header
     //     }).then((res) => res.json());
     // }
+
+    const pushCmt = async (data) => {
+        await fetch(api + 'comment', {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        }).then((res) => res.json());
+    }
+    
     const handle = {
         currentIndex: 0,
         isPlaying: false,
         isRandom: false,
         isRepeat: false,
+        isComment: false,
         config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
         songs: listSong,
         setconfig: function (key, value) {
@@ -74,10 +92,13 @@ const app = async () => {
                     <h3 class="title">${song.name}</h3>
                     <p class="author">${song.singer}</p>
                 </div>
-                <div class="option" style="color:while;">
-                    <a href="${api}download?id=${song.src}"><i class="fas fa-download"></i></a>
-                </div>
-                </div>`;
+                <ul class="option">
+                    <li><a href="${api}download?id=${song.src}"><i class="fas fa-download"></i></a></li>
+                    <li><a href="" onclick="handleClickComment(event,'${song._id}')"><i class="fa-solid fa-comment-dots"></i></a></li>
+
+                </ul>
+                </div>`
+                    ;
             });
             $('.playlist').innerHTML = htmls.join('');
         },
@@ -155,7 +176,7 @@ const app = async () => {
 
 
             //next song 
-            nextBtn.onclick =async function () {
+            nextBtn.onclick = async function () {
                 if (handle.isRandom) {
                     await handle.randomSong();
                 } else {
@@ -163,12 +184,12 @@ const app = async () => {
                 }
                 await handle.render();
                 await audio.play();
-                
+
                 await handle.scrollToActiveSong();
             }
 
             // prev song
-            prevBtn.onclick =async function () {
+            prevBtn.onclick = async function () {
                 if (handle.isRandom) {
                     await handle.randomSong();
                 } else {
@@ -176,7 +197,7 @@ const app = async () => {
                 }
                 await handle.render();
                 await audio.play();
-                
+
                 await handle.scrollToActiveSong();
             }
 
@@ -205,7 +226,7 @@ const app = async () => {
             }
 
             // lắng nghe hành vi click vào playlist
-            playlist.onclick =async function (e) {
+            playlist.onclick = async function (e) {
                 const songElement = e.target.closest('.song:not(.active)');
                 if (songElement || e.target.closest('.song .option')) {
                     // xử lí click khi ấn vào song option
@@ -220,6 +241,30 @@ const app = async () => {
                 }
             }
 
+            btncomment.onclick = () => {
+                const comment = $('.input-comment').value
+                const person = $('.input-name').value
+                if (comment == '' || person == '') {
+                    alert('Không được để trống!')
+                }
+                else {
+                    const newDiv = `<div class="cmt">
+                                        <div class="logo">
+                                            <img src="" alt="" class="comment-logo">
+                                        </div>
+                                        <div class="content">
+                                            <h3 class="username">${person}</h3>
+                                            <p class="des">${comment}</p>
+                                        </div>
+                                    </div>`
+                    
+                    pushCmt({comment, person, Music});
+                    cmtBody.insertAdjacentHTML('afterbegin', newDiv)
+                    $('.input-comment').value= '';
+                    $('.input-name').value= '';
+                }
+
+            }
         },
         // kéo phần active lên tầm nhìn
         scrollToActiveSong: function () {
@@ -235,7 +280,7 @@ const app = async () => {
             // console.log(thumb);
             thumb.style = `background-image: url(' ${this.currentSong.image}')`;
             // await sendRequest({name: this.currentSong.name})
-            audio.src = api+'stream?id='+ this.currentSong.src;
+            audio.src = api + 'stream?id=' + this.currentSong.src;
             await audio.load()
         },
         nextSong: async function () {
@@ -256,7 +301,7 @@ const app = async () => {
             } while (this.currentIndex === tmp);
             this.loadCurrentSong();
         },
-        start:async function () {
+        start: async function () {
             // load config
             await this.loadConfig();
 
@@ -275,6 +320,50 @@ const app = async () => {
         }
     }
     handle.start();
+
+
 }
 
 app();
+let isComment = false;
+let Music = '';
+
+const getCmt = async (id) =>{
+    Music = id;
+
+    await fetch(api + 'comment?id='+id)
+            .then((res) => res.json())
+            .then((res) => {
+                const htmls = res.map(comment => {
+                    return `<div class="cmt">
+                    <div class="logo">
+                        <img src="" alt="" class="comment-logo">
+                    </div>
+                    <div class="content">
+                        <h3 class="username">${comment.person}</h3>
+                        <p class="des">${comment.comment}</p>
+                    </div>
+                </div>`
+                })
+                return htmls;
+            })
+            .then((htmls) => {
+                cmtBody.innerHTML = htmls.join('')
+            })
+
+}
+
+const handleClickComment = (e, id) => {
+    e.preventDefault();
+    if (isComment == false) {
+        comment.classList.add('hide')
+        isComment = true;
+        getCmt(id);
+    }
+    else {
+        comment.classList.remove('hide')
+        isComment = false;
+    }
+}
+
+
